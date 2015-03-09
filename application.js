ac5eadf51ed776aceca721943c5ab6a4eff75678
Mark Eschbach <meschbach@gmail.com>
2015-03-09 10:45:12 -0700

    Token aquization and authorization works

--- application.js
+++ application.js
@@ -1,12 +1,31 @@
 var express = require("express");
-var jwt = require("express-jwt");
-var id = require("node-uuid").v4();
+var bodyParser = require("body-parser" );
+var express_jwt = require("express-jwt");
+var jwt = require("jsonwebtoken"); 
+var uuid = require("node-uuid");
+var morgan = require( "morgan" );
+
+var secret = uuid.v4();
+var flag = uuid.v4();
 
 var port = 8000;
 
 var app = express();
-app.get( "/flag", function( request, response ){
-	response.json({ flag: id });
+app.use( morgan( "short" ) );
+
+app.post( "/token", bodyParser.json(), function( request, response ){
+	if( request.body.user == 'hobbit' && request.body.secret == 'tolken' ){
+		var token = jwt.sign({ touch: 'stars' }, secret.toString() );
+		response.send( token );
+		response.end();
+	}else{
+		response.statusCode = 422; //Totally not the right code for auth; but hey!  it's a prototype
+		response.end();
+	}
+});
+
+app.get( "/flag", express_jwt({ secret: secret.toString() }), function( request, response ){
+	response.json({ flag: flag });
 });
 
 app.listen( port, function(){
--- client.js
+++ client.js
@@ -1,14 +1,27 @@
 var request = require( "request" );
+var base = "http://localhost:8000";
 
-request( "http://localhost:8000/flag", function( err, response, body ){
-	if( err ) {
-		return console.error( "Error: ", err );
-	}
+function rest_post( resource, body, next ){
+	return request({ method: "POST", uri: base + resource, json: body }, next);
+}
 
-	var code = response.statusCode;
-	if( code == 200 ){
-		console.log( JSON.parse( body ) );
-	}else{
-		console.error( "Status: ", code ); 
+rest_post( "/token", { user: "hobbit", secret: "tolken" }, function( err, response, body ){
+	if( err ){ return console.error( "request error: ", err.toString() ); }
+	if( response.statusCode != 200 ){
+		return console.error( "failed to get toekn", response.statusCode );
 	}
+	var token = body;
+
+	request({ uri: "http://localhost:8000/flag", headers: { 'Authorization': 'Bearer ' + token } }, function( err, response, body ){
+		if( err ) {
+			return console.error( "Error: ", err );
+		}
+
+		var code = response.statusCode;
+		if( code == 200 ){
+			console.log( JSON.parse( body ) );
+		}else{
+			console.error( "Status: ", code ); 
+		}
+	});
 });
