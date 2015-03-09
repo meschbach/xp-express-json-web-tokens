623a0f655333a52d05e46cfae7fbc903adeb78bc
Mark Eschbach <meschbach@gmail.com>
2015-03-09 10:50:47 -0700

    Annoting the code to make sections clearer

--- application.js
+++ application.js
@@ -1,3 +1,6 @@
+/*
+ * Dependencies
+ */
 var express = require("express");
 var bodyParser = require("body-parser" );
 var express_jwt = require("express-jwt");
@@ -5,17 +8,26 @@ var jwt = require("jsonwebtoken");
 var uuid = require("node-uuid");
 var morgan = require( "morgan" );
 
-var secret = uuid.v4();
+/*
+ * Generated (will remain as constants) 
+ */
+var jwtSigningSecret = uuid.v4();
 var flag = uuid.v4();
 
+/*
+ * Options
+ */
 var port = 8000;
 
+/*
+ * Express application assembly
+ */
 var app = express();
 app.use( morgan( "short" ) );
 
 app.post( "/token", bodyParser.json(), function( request, response ){
 	if( request.body.user == 'hobbit' && request.body.secret == 'tolken' ){
-		var token = jwt.sign({ touch: 'stars' }, secret.toString() );
+		var token = jwt.sign({ touch: 'stars' }, jwtSigningSecret.toString() );
 		response.send( token );
 		response.end();
 	}else{
@@ -24,10 +36,13 @@ app.post( "/token", bodyParser.json(), function( request, response ){
 	}
 });
 
-app.get( "/flag", express_jwt({ secret: secret.toString() }), function( request, response ){
+app.get( "/flag", express_jwt({ secret: jwtSigningSecret.toString() }), function( request, response ){
 	response.json({ flag: flag });
 });
 
+/*
+ * Listen for clients
+ */
 app.listen( port, function(){
 	console.log( "Listening on ", port );
 });
